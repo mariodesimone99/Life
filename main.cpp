11720041c64b6abdd484359d9363300be60f4504
nexus9971 <mario.desimone@stud.unifi.it>
2022-06-27 16:38:52 +0200

    Finale I

--- main.cpp
+++ main.cpp
@@ -1,19 +1,17 @@
-#include <iostream>
-#include <cmath>
 #include <random>
 #include <unordered_map>
-#include <stdio.h>
 
 const unsigned int width = 100;
 const unsigned int height = 100;
-const unsigned int nCells = width * height;
-const float density = 0.5;
-const float prob = 0.015;
+const unsigned int ncells = width * height;
+const float initialdensity = 0.5;
+const float prob = 0.028;
 const int ntry = 1;
 unsigned int livingcells = 0;
-const int maxiter = 1000000;
+const int maxiter = 10000;
 bool permutation = false;
 
+//Set a cell as alive and increment the count of neighbours cell alive of the neighbours
 void SetCell(unsigned int x, unsigned int y, unsigned char* life){
     *(life+x*width+y) |= 0x01;
     int yright, yleft, xabove, xbelow;
@@ -28,11 +26,11 @@ void SetCell(unsigned int x, unsigned int y, unsigned char* life){
         yright = 1;
     }
     if (x==0){
-        xabove = nCells-width;
+        xabove = ncells - width;
         xbelow = width;
     }else if (x == height-1){
         xabove = -width;
-        xbelow = -(nCells-width);
+        xbelow = -(ncells - width);
     }else{
         xabove = -width;
         xbelow = width;
@@ -48,6 +46,7 @@ void SetCell(unsigned int x, unsigned int y, unsigned char* life){
     livingcells++;
 }
 
+//Set a cell as dead and decrement the count of neighbours cell alive of the neighbours
 void ClearCell(unsigned int x, unsigned int y, unsigned char* life){
     *(life+x*width+y) &= ~0x01;
     int yright, yleft, xabove, xbelow;
@@ -62,11 +61,11 @@ void ClearCell(unsigned int x, unsigned int y, unsigned char* life){
         yright = 1;
     }
     if (x==0){
-        xabove = nCells-width;
+        xabove = ncells - width;
         xbelow = width;
     }else if (x == height-1){
         xabove = -width;
-        xbelow = -(nCells-width);
+        xbelow = -(ncells - width);
     }else{
         xabove = -width;
         xbelow = width;
@@ -82,14 +81,13 @@ void ClearCell(unsigned int x, unsigned int y, unsigned char* life){
     livingcells--;
 }
 
+//Plot the game in the command line
 void Draw(unsigned char* life){
     for (unsigned int i = 0; i < height; ++i) {
         for(unsigned int j = 0; j < width; ++j){
             if ((*(life+j+i*width)&0x01)==0){
-                //printf("o%d\t", life[i*width+j]>>1);
                 printf("o\t");
             }else{
-                //printf("*%d\t", life[i*width+j]>>1);
                 printf("*\t");
             }
         }
@@ -97,13 +95,14 @@ void Draw(unsigned char* life){
     }
 }
 
+//Next iteration of the base game of life
 void NextGeneration(unsigned char* life){
     unsigned char* tmp;
-    tmp = (unsigned char*)malloc(nCells*sizeof(unsigned char));
-    memcpy(tmp, life, nCells*sizeof(unsigned char));
+    tmp = (unsigned char*)malloc(ncells * sizeof(unsigned char));
+    memcpy(tmp, life, ncells * sizeof(unsigned char));
     int i=0, j=0, k=0;
-    while(k < nCells) {
-        while(*(tmp)==0 && k < nCells-1){//Dead and no neighbours
+    while(k < ncells) {
+        while(*(tmp)==0 && k < ncells - 1){//Dead and no neighbours
             k++;
             tmp++;
             j++;
@@ -131,16 +130,17 @@ void NextGeneration(unsigned char* life){
     }
 }
 
+//Next iteration of the game of life but putting a cell a dead with a given probability
 void NextGenerationZero(unsigned char* life, float prob){
     std::random_device rd;  // Will be used to obtain a seed for the random number engine
     std::mt19937 generator(rd()); // Standard mersenne_twister_engine seeded with rd()
     std::uniform_real_distribution<float>  distr(0, 1);
 
     unsigned char* tmp;
-    tmp = (unsigned char*)malloc(nCells*sizeof(unsigned char));
-    memcpy(tmp, life, nCells*sizeof(unsigned char));
+    tmp = (unsigned char*)malloc(ncells * sizeof(unsigned char));
+    memcpy(tmp, life, ncells * sizeof(unsigned char));
     int i=0, j=0, k=0;
-    while(k < nCells) {
+    while(k < ncells) {
         if (distr(generator) <= prob){
             if (((*(tmp))&0x01)==1){
                 ClearCell(i, j, life);
@@ -173,16 +173,17 @@ void NextGenerationZero(unsigned char* life, float prob){
     }
 }
 
+//Next iteration of the game of life but the state of the cell with a given probability
 void NextGenerationIdentity(unsigned char* life, float prob){
     std::random_device rd;  // Will be used to obtain a seed for the random number engine
     std::mt19937 generator(rd()); // Standard mersenne_twister_engine seeded with rd()
     std::uniform_real_distribution<float>  distr(0, 1);
 
     unsigned char* tmp;
-    tmp = (unsigned char*)malloc(nCells*sizeof(unsigned char));
-    memcpy(tmp, life, nCells*sizeof(unsigned char));
+    tmp = (unsigned char*)malloc(ncells * sizeof(unsigned char));
+    memcpy(tmp, life, ncells * sizeof(unsigned char));
     int i=0, j=0, k=0;
-    while(k < nCells) {
+    while(k < ncells) {
         if (distr(generator) > prob) {
             if (((*(tmp)) & 0x01) == 0) {//Dead but possibly to born
                 if ((*(tmp)) >> 1 == 3) {
@@ -212,20 +213,22 @@ void NextGenerationIdentity(unsigned char* life, float prob){
     }
 }
 
+//We use this to get a sequnce of char that represent the entire map, it s used with hash to check loops
 std::string PatternReduce(unsigned char* life){
     std::string temp = "";
-    for (int i = 0; i < nCells; ++i) {
+    for (int i = 0; i < ncells; ++i) {
         temp += (*(life+i))&0x01;
     }
     return temp;
 }
 
+//We permute the state of a cell near one alive
 void Permutation(unsigned char* life){
     //Check if it has sense the permutation
     if (livingcells > 1){
         unsigned char* tmp;
-        tmp = (unsigned char*)malloc(nCells*sizeof(unsigned char));
-        memcpy(tmp, life, nCells*sizeof(unsigned char));
+        tmp = (unsigned char*)malloc(ncells * sizeof(unsigned char));
+        memcpy(tmp, life, ncells * sizeof(unsigned char));
         int index=0;
         while (((*tmp)&0x01)==1 || (*(tmp))>>1==0){
             tmp++;
@@ -239,8 +242,9 @@ void Permutation(unsigned char* life){
     }
 }
 
-void Visualize(FILE *gp, unsigned char* life, int k, int iterations) {
-    fprintf(gp, "set title 't=%d'\n", iterations);
+//Plot with gnuplot of the game
+void Visualize(FILE *gp, unsigned char* life, int iterations) {
+    fprintf(gp, "set title 'Time=%d Living Cells=%d'\n", iterations, livingcells);
     fprintf(gp, "plot '-' w image\n");
     for (int i=0; i<height; i++) {
         for (int j=0; j<width; j++) {
@@ -248,58 +252,56 @@ void Visualize(FILE *gp, unsigned char* life, int k, int iterations) {
         }
         fprintf(gp, "\n");
     }
-    //fprintf(gp, "'pause -1'\n");
     fprintf(gp, "e\n");
     fflush(gp);
 }
 
+//Plot with gnuplot of the average of alive cells in the game in a logscale
 void Graph(FILE *gp, float* densityvect){
     fprintf(gp, "set title 'Average Living Cells'\n");
     fprintf(gp, "set logscale xy\n");;
     fprintf(gp, "plot '-' w lines\n");
     for (int i = 0; i < maxiter; ++i) {
-        if (densityvect[i] != 0){
-            fprintf(gp, "%d %f\n",i, densityvect[i]/ntry);
-        }
+        fprintf(gp, "%d %f\n",i, densityvect[i]/ntry);
     }
-    //fprintf(gp, "'pause -1'\n");
-    fprintf(gp, "set term png\n");
-    fprintf(gp, "set output 'graph.png'\n");
     fprintf(gp, "e\n");
     fflush(gp);
 }
 
 int main() {
+    //Hash table to check if we get the same pattern more times
     std::unordered_map<std::string, unsigned int> patterns;
+    //Pointer of the grid
     unsigned char* life;
-    life = (unsigned char*)malloc(nCells*sizeof(unsigned char));
+    life = (unsigned char*)malloc(ncells * sizeof(unsigned char));
     float* densityvect = (float*) malloc(maxiter*sizeof(float));
     memset(densityvect, 0, maxiter*sizeof(float));
     std::random_device rd;  // Will be used to obtain a seed for the random number engine
     std::mt19937 generator(rd()); // Standard mersenne_twister_engine seeded with rd()
     std::uniform_real_distribution<float>  distr(0, 1);
     FILE *gp;
-    gp = popen("gnuplot", "w");
+    gp = popen("gnuplot -persist", "w");
     fprintf(gp, "unset key; set cbrange [0:1]\n");
+    FILE *gr;
+    gr = popen("gnuplot -persist", "w");
+    fprintf(gr, "unset key; set cbrange [0:1]\n");
 
     for (int n = 0; n < ntry; ++n) {
         float* densityptr = densityvect;
-        memset(life, 0, nCells * sizeof(unsigned char));
+        memset(life, 0, ncells * sizeof(unsigned char));
+        //Initialization of the grid
         for (unsigned int i = 0; i < height; ++i) {
             for (unsigned int j = 0; j < width; ++j) {
-                if (distr(generator) <= density) {
+                if (distr(generator) <= initialdensity) {
                     SetCell(i, j, life);
                 }
             }
         }
         patterns[PatternReduce(life)] = 0;
         //Draw(life);
-        Visualize(gp, life, 5, 0);
+        Visualize(gp, life, 0);
         *densityptr += livingcells;
         densityptr++;
-        double d = livingcells / ((double) nCells);
-        printf("Density living cells: %f",d);
-        printf("\n\n");
         int k = 0;
         bool loop = false;
         do {
@@ -308,11 +310,8 @@ int main() {
             NextGenerationZero(life, prob);
             *densityptr += livingcells;
             densityptr++;
-            Visualize(gp, life, 5, k + 1);
+            Visualize(gp, life, k + 1);
             //Draw(life);
-            d = livingcells / ((double) nCells);
-            printf("Density living cells: %f", d);
-            printf("\n\n");
             std::string temp = PatternReduce(life);
             //Cerchiamo nella tabella hash se la configurazione è già presente, altrimenti la inseriamo
             if (patterns.find(temp) == patterns.end()) {
@@ -325,33 +324,23 @@ int main() {
                 printf("Loop found after %d iterations\n", k);
                 printf("Let's permutate\n");
                 patterns.clear();
-                *densityptr = d;
+                *densityptr += livingcells;
                 densityptr++;
                 k = 0;
                 Permutation(life);
-                Draw(life);
+                Visualize(gp, life, k + 1);
+                //Draw(life);
                 loop = false;
                 permutation = false;
             }
         } while (!loop && k<maxiter);
+        printf("Asymptotic state obtained in %d iterations\n", k);
         patterns.clear();
         livingcells = 0;
-        permutation = true;
-        if (k < maxiter){
+        permutation = false;
+        if (k < maxiter && permutation){
             printf("Asymptotic state retarded by %d iterations", k);
-        }else{
-            printf("Max iterations exceeded");
         }
     }
-    printf("\n");
-    fprintf(gp, "clear\n");
-    while(true){
-        Graph(gp, densityvect);
-    }
-
-    /*for (int i = 0; i < maxiter; ++i) {
-        if (densityvect[i] != 0){
-            printf("%f - %f\n", densityvect[i]/ntry, log10(densityvect[i]/ntry));
-        }
-    }*/
+    Graph(gr, densityvect);
 }
